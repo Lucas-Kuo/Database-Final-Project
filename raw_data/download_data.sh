217cb70491721048e77d4c19eea0f3d9d768be11
Lucas Kuo <74166753+Lucas-Kuo@users.noreply.github.com>
2022-06-25 18:18:05 +0800

    Create download_data.sh

---
+++ raw_data/download_data.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wget https://nyc-tlc.s3.amazonaws.com/trip+data/yellow_tripdata_2022-01.parquet
+wget https://nyc-tlc.s3.amazonaws.com/trip+data/yellow_tripdata_2022-02.parquet
+wget https://nyc-tlc.s3.amazonaws.com/trip+data/yellow_tripdata_2022-03.parquet
